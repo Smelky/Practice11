f9b709afa387b1b203751a4635a71e6657e830af
Smelky <46086711+Smelky@users.noreply.github.com>
2019-03-30 21:40:40 +0200

    changed

--- src/test/java/TestFruitShop.java
+++ src/test/java/TestFruitShop.java
@@ -7,7 +7,7 @@ public class TestFruitShop {
     FruitShop fruitShop;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         fruitShop = new FruitShop();
         fruitShop.addFruits("src/main/resources/FirstDelivery.json");
         fruitShop.save("src/main/resources/AllFruitData.json");
@@ -16,27 +16,46 @@ public class TestFruitShop {
     }
 
     @Test
-    public void testGetSpoiledAndAvailableFruits(){
-        int numOfSpoiledFruits = fruitShop.getSpoiledFruits("24-05-2012").size();
+    public void testGetAvailableFruits() {
         int numOfAvailableFruits = fruitShop.getAvailableFruits("24-05-2012").size();
-        assertEquals((numOfSpoiledFruits + numOfAvailableFruits), 14);
+        assertEquals((numOfAvailableFruits), 12);
     }
 
     @Test
-    public void testGetSpoiledAndAvailableFruitsAdvanced(){
-        int numOfSpoiledFruits =  fruitShop.getSpoiledFruits("24-05-2012", TypeOfFruit.KIWI).size();
+    public void testGetSpoiledFruits() {
+        int numOfSpoiledFruits = fruitShop.getSpoiledFruits("24-05-2012").size();
+        assertEquals((numOfSpoiledFruits), 2);
+
+    }
+
+    @Test
+    public void testGetSpoiledAndAvailableFruitsAdvanced() {
+        int numOfSpoiledFruits = fruitShop.getSpoiledFruits("24-05-2012", TypeOfFruit.KIWI).size();
         int numOfAvailableFruits = fruitShop.getAvailableFruits("24-05-2012", TypeOfFruit.KIWI).size();
         assertEquals((numOfSpoiledFruits + numOfAvailableFruits), 3);
     }
 
     @Test
-    public void testGetAddedFruits(){
+    public void testGetAvailableFruitsAdvanced() {
+        int numOfAvailableFruits = fruitShop.getAvailableFruits("24-05-2012", TypeOfFruit.KIWI).size();
+        assertEquals((numOfAvailableFruits), 2);
+    }
+
+    @Test
+    public void testGetSpoiledFruitsAdvanced() {
+        int numOfSpoiledFruits = fruitShop.getSpoiledFruits("24-05-2012", TypeOfFruit.KIWI).size();
+        assertEquals((numOfSpoiledFruits), 1);
+
+    }
+
+    @Test
+    public void testGetAddedFruits() {
         int numOfFruits = fruitShop.getAddedFruits("18-05-2012").size();
         assertEquals((numOfFruits), 3);
     }
 
     @Test
-    public void testGetAddedFruitsAdvanced(){
+    public void testGetAddedFruitsAdvanced() {
         int numOfFruits = fruitShop.getAddedFruits("23-05-2012", TypeOfFruit.KIWI).size();
         assertEquals((numOfFruits), 1);
     }
